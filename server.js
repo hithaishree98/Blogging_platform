8073cf04d375e84c73c9cc69ab19e754dd3c3726
Glitch (glitch-hello-website) <none>
2024-12-13 19:49:58 +0000

    🛬😚 Checkpoint ./server.js:70028187/1:70028192/820 ./package.json:70028192/33

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const dotenv = require('dotenv');
 const connectToDatabase = require('./config/database'); // Import database connection logic
 const path = require('path');
 const authRoutes = require("./routes/auth");
-//const adminRoutes = require("./routes/admin");
-const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/users");
 const app = express();
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
 
 // Load environment variables
 dotenv.config();
 
+console.log("adminRoutes:", adminRoutes);
+app.set("view engine", "ejs");
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,8 +36,17 @@ app.use((req, res) => {
 app.use("/auth", authRoutes);
 //app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
+app.use("/admin", adminRoutes);
 
 // Start server
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`App is listening on port ${listener.address().port}`);
 });
+
+
+
+
+// Middleware setup
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(methodOverride("_method")); // Enable support for PUT/DELETE in forms
