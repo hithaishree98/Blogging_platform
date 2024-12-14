6c1d3a1e3b6e0eca467808e1b23105cae774cf00
Glitch (glitch-hello-website) <none>
2024-12-14 02:30:53 +0000

    👨🐄 Checkpoint ./public/css/admin.css:70028192/295 ./models/Blog.js:70028192/438

--- models/Blog.js
+++ models/Blog.js
@@ -7,7 +7,10 @@ const blogSchema = new mongoose.Schema({
   likes: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
   rating: { type: Number, default: 0 },
-  destination: { type: String, required: true }
+  destination: { type: String, required: true },
+  imageUrl: {type: String, // URL to the image (string type)
+    required: false // This can be optional, depending on your requirements
+  }
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
