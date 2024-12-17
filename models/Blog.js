0bd6a893ebbb2bde9f8ca76673eac9059c0dd030
Glitch (glitch-hello-website) <none>
2024-12-17 07:37:57 +0000

    🐢👛 Checkpoint ./models/Blog.js:70028187/1304:70133810/1940 ./server.js:70028187/2152

--- models/Blog.js
+++ models/Blog.js
@@ -4,13 +4,11 @@ const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  likes: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
-  rating: [{ user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, score: Number }],
+  rating: { type: Number, default: 0 },
   destination: { type: String, required: true },
   imageUrl: {type: String, required: false },
   saved: { type: Boolean, default: false },
-  savedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], 
+  savedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Users who saved this blog
 });
-
-module.exports = mongoose.model('Blog', blogSchema);
