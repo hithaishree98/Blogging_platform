0be8a8ad27f077176ca52586c3833e26d60024c0
Glitch (glitch-hello-website) <none>
2024-12-17 06:15:46 +0000

    🍙🎒 Checkpoint ./public/css/style.css:70133810/17151 ./public/css/profile.css:70028187/31:70133810/7029 ./models/Blog.js:70028192/226 ./views/profile.ejs:70028187/5415:70133810/1903 ./views/edit-profile.ejs:70133810/5996

--- models/Blog.js
+++ models/Blog.js
@@ -11,11 +11,6 @@ const blogSchema = new mongoose.Schema({
   imageUrl: {type: String, required: false },
   saved: { type: Boolean, default: false },
   savedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Users who saved this blog
-  comments:[
-    {
-    user: { type: mongoose. Schema. Types. ObjectId, ref: 'User' }, text: { type: String, required: true } , createdAt: {type: Date, default: Date.now }
-    }
-  ]
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
